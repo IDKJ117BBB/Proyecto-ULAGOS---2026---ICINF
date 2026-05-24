07c5c26cef45f045c7e78af5bc72c9c152448463
Samuel <samuelmzarallo@outlook.com>
2026-05-24 01:48:05 -0400

    Modelos (sin terminar)+requerimientos

---
+++ Modelos.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__) #__name__ hace referencia al archivo en el q estamos
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///Maindatabase.db"#Configuracion de los motores
+
+#inicializar SQLalchemy
+databaseCAFETERIA = SQLAlchemy()#Instanciar un objeto database
+databaseCAFETERIA.init_app(app)
+
+"""class base(DeclarativeBase):
+    pass
+"""
+#Cada modelo es una tabla en la base de datos
+
+#Clase para usuario y admin
+class User(databaseCAFETERIA.Model):
+    id= databaseCAFETERIA.Column(databaseCAFETERIA.Integer, primary_key=True)
+    username = databaseCAFETERIA.Column(databaseCAFETERIA.String(60), nullable=False)
+    password = databaseCAFETERIA.Column(databaseCAFETERIA.String(30), nullable=False)
+    role = databaseCAFETERIA.Column(databaseCAFETERIA.Boolean, nullable=False, default=False)#rol, True=admin, False=cliente
+    #Añadir métodos que usen los atributos de user
+    #Tales como iniciar sesión, realizar reserva
+    def user_verification(self, passwordIn): #PasswordIn es la contraseña que le pasaremos desde la ventana
+        """Logica aqui"""
+        return
+    def agregar_usuario(self): #Se supone solo el admin debe tener acceso a esto
+        if self.role==1:
+            #Crea un usuario y lo agrega
+            #Aqui va lo que puede hacer el admin (en este caso agregar user)
+            return
+    def makereservation(self, menuIds, comidaId):
+        return
+    
+#Clase para almuerzo
+class Lunch(databaseCAFETERIA.Model):
+    id = databaseCAFETERIA.Column(databaseCAFETERIA.Integer, primary_key=True)
+    name = databaseCAFETERIA.Column(databaseCAFETERIA.String(80), nullable=False)
+    quantity = databaseCAFETERIA.Column(databaseCAFETERIA.Integer)#Cantidad de esa comida en especifico
+    #description = db.Column(db.Text)
+    menu_id = databaseCAFETERIA.Column(databaseCAFETERIA.Integer, databaseCAFETERIA.ForeignKey('menu.id'), nullable=False) #Clave foránea de la tabla menú
+    order = databaseCAFETERIA.Column(databaseCAFETERIA.Integer, unique=True, nullable=False) #Numero de orden
+    #Métodos para enlazarse con un Menu
+    def addtomenu(self, menuID):
+        return
+    #Métodos para borrar o editar alguna comida o agregar
+    
+class Menu(databaseCAFETERIA.Model): #Falta crear tabla intermedia entre Food y Menu (N a N)
+    id = databaseCAFETERIA.Column(databaseCAFETERIA.Integer, primary_key=True)
+    day = databaseCAFETERIA.Column(databaseCAFETERIA.String(20), nullable=False)
+    #time = db.Column(db.String(20), nullable=False)
+    #enu = db.Column(db.Text, nullable=False)
+    #foods = db.relationship('Food', backref='menu', lazy=True, cascade='all, delete-orphan')
+    
+class Reservation(databaseCAFETERIA.Model): # N a 1 con User
+    id = databaseCAFETERIA.Column(databaseCAFETERIA.Integer, primary_key=True)
+    
+class groceries(databaseCAFETERIA.Model):
+    id = databaseCAFETERIA.Column()
