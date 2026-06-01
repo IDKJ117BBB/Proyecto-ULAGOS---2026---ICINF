96fca114603f57c82bb89c80ef5073a3d4b11fd0
Samuel <samuelmzarallo@outlook.com>
2026-05-31 23:05:08 -0400

    Agregar algunos atributos, preparar relaciones tablas intermedias

--- Modelos.py
+++ Modelos.py
@@ -5,8 +5,8 @@ app=Flask(__name__) #__name__ hace referencia al archivo en el q estamos
 app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///Maindatabase.db"#Configuracion de los motores
 
 #inicializar SQLalchemy
-databaseCAFETERIA = SQLAlchemy()#Instanciar un objeto database
-databaseCAFETERIA.init_app(app)
+db = SQLAlchemy()#Instanciar un objeto database
+db.init_app(app)
 
 """class base(DeclarativeBase):
     pass
@@ -14,11 +14,12 @@ databaseCAFETERIA.init_app(app)
 #Cada modelo es una tabla en la base de datos
 
 #Clase para usuario y admin
-class User(databaseCAFETERIA.Model):
-    id= databaseCAFETERIA.Column(databaseCAFETERIA.Integer, primary_key=True)
-    username = databaseCAFETERIA.Column(databaseCAFETERIA.String(60), nullable=False)
-    password = databaseCAFETERIA.Column(databaseCAFETERIA.String(30), nullable=False)
-    role = databaseCAFETERIA.Column(databaseCAFETERIA.Boolean, nullable=False, default=False)#rol, True=admin, False=cliente
+class User(db.Model):
+    __tablename__='user'
+    id= db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    role = db.Column(db.Boolean, nullable=False, default=False)#rol, True=admin, False=cliente
     #Añadir métodos que usen los atributos de user
     #Tales como iniciar sesión, realizar reserva
     def user_verification(self, passwordIn): #PasswordIn es la contraseña que le pasaremos desde la ventana
@@ -33,37 +34,53 @@ class User(databaseCAFETERIA.Model):
         return
     
 #Clase para almuerzo (item)
-class Lunch(databaseCAFETERIA.Model):
-    id = databaseCAFETERIA.Column(databaseCAFETERIA.Integer, primary_key=True)
-    name = databaseCAFETERIA.Column(databaseCAFETERIA.String(80), nullable=False)
-    quantity = databaseCAFETERIA.Column(databaseCAFETERIA.Integer)#Cantidad de esa comida en especifico
-    #description = db.Column(db.Text)
+class Lunch(db.Model):
+    __tablename__='lunch'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    quantity = db.Column(db.Integer)#Cantidad de esa comida en especifico
+    description = db.Column(db.String(200), nullable=True)
     #Métodos para enlazarse con un Menu
     def addtomenu(self, menuID):#Relacionar con la tabla intermedia
         return
     #Métodos para borrar o editar alguna comida o agregar
     
 #Clase para Menu (tiene varios almuerzos)
-class Menu(databaseCAFETERIA.Model): #Falta crear tabla intermedia entre Food y Menu (N a N)
-    id = databaseCAFETERIA.Column(databaseCAFETERIA.Integer, primary_key=True)
-    day = databaseCAFETERIA.Column(databaseCAFETERIA.String(20), nullable=False)
+class Menu(db.Model):
+    __tablename__='menu'
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.String(20), nullable=False)
     #time = db.Column(db.String(20), nullable=False)
     #enu = db.Column(db.Text, nullable=False)
     #foods = db.relationship('Food', backref='menu', lazy=True, cascade='all, delete-orphan')
     
 #Clase para abarrotes
-class groceries(databaseCAFETERIA.Model):
-    id = databaseCAFETERIA.Column()
+class groceries(db.Model):
+    __tablename__='groceries'
+    id = db.Column()
+    name = db.Column()
+    quantity = db.Column(db.Integer)#Cantidad de producto en específico
+    description = db.Column(db.String(200), nullable=True)
 
 #######TABLAS RESERVAS###########
-class Reservation_lunch(databaseCAFETERIA.Model): # N a 1 con User y 1 a 1 con menú
-    id = databaseCAFETERIA.Column(databaseCAFETERIA.Integer, primary_key=True)
+class Reservation_lunch(db.Model): # N a 1 con User y 1 a 1 con menú
+    __tablename__='reservation_lunch'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    lunch_menu_id = db.Column(db.Integer, db.ForeignKey())
      
-class Reservation_groceries(databaseCAFETERIA.Model):
-    id=None
-    
-########TABLAS INTERMEDIAS########
+class Reservation_groceries(db.Model):
+    __tablename__='reservation_groceries'
+    id=db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=True)
 
+
+########TABLAS INTERMEDIAS########
 #Un menu puede tener varios almuerzos y un almuerzo puede aparecer en multiples menus
-class Lunch_Menu(databaseCAFETERIA.Model):
-    id=None
+class Lunch_Menu(db.Model):
+    __tablename__='lunch_menu'
+    id = db.Column(db.Integer, primary_key=True)
+    lunch_id = db.Column(db.Integer, db.ForeignKey('lunch.id'), nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'), nullable=False)
+
